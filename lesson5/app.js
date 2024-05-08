c88104a6226bae822f329fceb83e12a61a0fcd53
ZirikeN <129564930+ZirikeN@users.noreply.github.com>
2024-05-08 16:12:57 +0300

    js

---
+++ lesson5/app.js
@@ -0,0 +1,156 @@
+// let age = 20
+
+// function myAge(myAge){
+//     if(myAge>=18){
+//         console.log('Вы совершеннолетний')
+//     }else{
+//         console.log('Вы не совершеннолетний')
+//     }
+// }
+//===================================================================================
+// function myAge(myAge){
+//     (myAge>=18) ? console.log('Вы совершеннолетний') : console.log('Вы не совершеннолетний')
+// }
+//====================================================================
+// console.log(myAge(age))
+// let i = 2
+// while(i<=12){
+//     console.log(i)
+//     i = i + 2
+// }
+//=====================================================================================
+// let i = 10
+// while(i>0){
+//     console.log(i)
+//     i--
+// }
+//===================================================================================
+// for(let i = 2; i<=12; i = i + 2){
+//     console.log(i)
+// }
+// for(let i = 10; i>0; i--){
+//     console.log(i)
+// }
+
+//========================================================================================
+// const arr = [2, 5, 6, 1, 4, 6, 9, 0, 8, 6, 7, 0, 0]
+
+// for(let i = 0; i<arr.length; i++){
+//     if(arr[i]%2 == 0){
+//         console.log(arr[i])
+//     }
+// }
+
+//================================================================================================
+// const arr = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Dark Blue', 'Purple']
+
+// for(let i = arr.length-1; i>=0; i--){
+//     console.log(arr[i])
+// }
+//========================================================================================
+// let name = 'Dima'
+// function hello(myName){
+//     return `Hello ${myName}`
+// }
+
+// console.log(hello(name))
+//===========================================================================================
+// const arr = [1, 2, 54, 75, 87, 21, 6, 8, 9, 435, 65, 5, 65, 90, 9]
+
+// function myArray(array){
+//     for(let i = 0; i < array.length; i++){
+//         if(array[i] > 10){
+//             console.log(array[i])
+//         }
+//     }
+// }
+
+// myArray(arr)
+// ==========================================================
+// let a = 10
+// let b = 2
+
+// function calculator(firstNumber, secondNumber, sign){
+//     switch(sign){
+//         case 'plus':
+//             console.log(firstNumber + secondNumber)
+//             break
+//         case 'minus':
+//             console.log(firstNumber - secondNumber)
+//             break
+//         case 'multiply':
+//             console.log(firstNumber * secondNumber)
+//             break
+//         case 'divide':
+//             console.log(firstNumber / secondNumber)
+//             break
+//         default:
+//             console.log('Проверьте введённые данные')
+//     }
+// }
+
+// calculator(a, b, 'divide')
+
+//==================================================================================
+// const myInfo = {
+//     name: 'Dima',
+//     surName: 'Lazarev',
+//     age: 19
+// }
+
+// console.log(myInfo)
+//=====================================================================================
+
+// const myInfo = {
+//     name: 'Dima',
+//     surName: 'Lazarev',
+//     age: 19,
+//     sayHello(arg){
+//         console.log(`Hello ${arg}`)
+//     }
+// }
+
+// console.log(myInfo.sayHello('Ron'))
+
+//============================================================================
+
+// let count = 0
+
+// const info = [
+//     {
+//         name: 'Dima',
+//         surName: 'Lazarev',
+//         age: 19,
+//         isAdmin: true
+//     },
+//     {
+//         name: 'Oleg',
+//         surName: 'Mamaev',
+//         age: 29,
+//         isAdmin: false
+//     },
+//     {
+//         name: 'Anton',
+//         surName: 'Voronov',
+//         age: 32,
+//         isAdmin: false
+//     },
+//     {
+//         name: 'John',
+//         surName: 'Fall',
+//         age: 10,
+//         isAdmin: true
+//     }
+// ]
+
+// for(let i = 0; i<info.length; i++){
+//     if(info[i].isAdmin == false){
+//         count++
+//     }
+// }
+
+// console.log(count)
+
+//==================================================================
+
+
